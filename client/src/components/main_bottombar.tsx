2032cb93819e919ca5912af4ca602bcfca482f01
dltndn <james98099@gmail.com>
2023-02-14 16:50:23 +0900

    feat: add useNavigate

--- client/src/components/main_bottombar.tsx
+++ client/src/components/main_bottombar.tsx
@@ -1,5 +1,6 @@
 import {Icon} from 'semantic-ui-react';
 import styles from "../css/main_bottombar.module.css"
+import { useNavigate } from "react-router-dom";
 
 function GoRequestPage(){
     window.location.href="/request"
@@ -18,12 +19,13 @@ function GoInformationPage(){
 }
 
 function Main_Bottombar() {
+    const navigate = useNavigate();
 
     return (
         <div className={styles.btposition}>
            <ul className={styles.btul}>
                 <li>
-                    <button onClick={GoRequestPage}>
+                    <button onClick={() => navigate(`request`)}>
                         <span className={styles.icon}>
                         <Icon link name = 'plus square outline'/></span>
                         <span>의뢰하기</span>
