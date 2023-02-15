2a824883076bec23fddb7f97c0e9107a7b056a1e
dltndn <james98099@gmail.com>
2023-02-15 16:42:17 +0900

    feat: added crypto wallet provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,14 @@
+import { WagmiConfig, createClient, configureChains } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal, useWeb3ModalTheme } from "@web3modal/react";
+import { projectId } from "./contractInformation";
+import { Buffer } from "buffer";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/mainPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -7,19 +18,56 @@ import ChattingPage from "./pages/ChattingPage";
 import ProfilePage from "./pages/ProfilePage";
 import React from "react";
 
+Buffer.from("anything", "base64");
+window.Buffer = window.Buffer || require("buffer").Buffer;
+
+const chains = [polygonMumbai];
+
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId }),
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({
+    projectId,
+    version: "1",
+    appName: "web3Modal",
+    chains,
+  }),
+  provider,
+});
+
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 function App() {
+  const { theme, setTheme } = useWeb3ModalTheme();
+  setTheme({
+    themeMode: "dark",
+    themeColor: "default",
+    themeBackground: "gradient",
+  });
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/signUp" element={<SignUpPage />} />
-        <Route path="/request" element={<RequestPage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/chatting" element={<ChattingPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/signUp" element={<SignUpPage />} />
+            <Route path="/request" element={<RequestPage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/chatting" element={<ChattingPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Routes>
+        </BrowserRouter>
+      </WagmiConfig>
+      <Web3Modal
+        projectId={projectId}
+        ethereumClient={ethereumClient}
+        themeBackground={theme.themeBackground}
+      />
+    </>
   );
 }
 
