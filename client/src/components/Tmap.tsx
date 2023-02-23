e514b7bb091b03533790150d2c8dc907a0f036e2
Daniel-k-dev <juyeon10120@naver.com>
2023-02-24 02:41:54 +0900

    feat : map zoom work

--- client/src/components/Tmap.tsx
+++ client/src/components/Tmap.tsx
@@ -2,54 +2,60 @@ import React, { createElement, useEffect, useState } from 'react';
 import ReactDOM from "react-dom";
 import Map from "../script/Tmap"
 
-
-// @ts-ignore
-const { Tmapv3 } = window;
-
-interface basicPos {
-    latitude?: number,
-    longitude?: number
-}
-
-interface Position {
-    containerId: string
-    startPosition: basicPos
-    arrivePosition: basicPos
-}
-    
-interface position {
-    latitude?: number,
-    longitude?: number
-}
-
-interface props {
-    containerId: string
-    startPosition: position
-    arrivePosition: position
-}
-
 const Tmap = ({ containerId, startPosition, arrivePosition }: props) => {
-    
-    const [map , setMap] = useState({})
-    const [startMarker , setStartMarker] = useState({})
-    const [arriveMarker , setArriveMarker] = useState({})
+
+    const [map, setMap] = useState({})
+    const [pos, setPos] = useState<position>({})
+    const [startMarker, setStartMarker] = useState({})
+    const [arriveMarker, setArriveMarker] = useState({})
+    const [zoomLevel, setZoomLevel] = useState(13)
+    const [markersBound, setMarkersBound] = useState({})
+    const [mapDIV, setMapDIV] = useState({})
 
     useEffect(() => {
         setMap(Map.initTmap())
+        
     }, [])
 
     useEffect(() => {
-        console.log(startPosition, arrivePosition)
         if (startPosition.latitude && startPosition.longitude) {
             setStartMarker(Map.Marker(map, startPosition.latitude, startPosition.longitude))
             Map.setViewMap(map, startPosition.latitude, startPosition.longitude);
             if (arrivePosition.latitude && arrivePosition.longitude) {
-                setArriveMarker(Map.Marker(map, arrivePosition.latitude, arrivePosition.longitude))
                 Map.setViewMap(map, (startPosition.latitude + arrivePosition.latitude) / 2, (arrivePosition.longitude + startPosition.longitude) / 2);
+                setArriveMarker(Map.Marker(map, arrivePosition.latitude, arrivePosition.longitude))
+                
+                // 중심점 기준 왼쪽 찾음    
+                if (startPosition.longitude < arrivePosition.longitude) {
+                    
+                    setPos({ latitude: startPosition.longitude, longitude: startPosition.latitude })
+                    
+                    console.log("markers : " ,startPosition.longitude, startPosition.latitude, arrivePosition.longitude, arrivePosition.latitude)
+                    Map.test(map, (startPosition.latitude + arrivePosition.latitude) / 2, (arrivePosition.longitude + startPosition.longitude) / 2, 
+                    startPosition.latitude, startPosition.longitude, arrivePosition.latitude, arrivePosition.longitude)
+                    
+                    // setMarkersBound(Map.initBound(startPosition.longitude, startPosition.latitude, arrivePosition.longitude, arrivePosition.latitude))
+                }
+                else {
+                    setPos({ latitude: arrivePosition.longitude, longitude: arrivePosition.latitude })
+                    // setMapDIV(Map.getBound(map))
+                }
             }
         }
     }, [startPosition, arrivePosition])
 
+    const onClick = () => {
+        //ok
+        // Map.test(map)
+        
+        // console.log("markers bound : ", markersBound)
+        // console.log("map bound : ",Map.getBound(map))
+        // Map.fitmap(map,markersBound,10)
+
+        //no
+        // console.log(Map.containMarker(Map.getBound(map), markersBound))
+        // setMapDIV(Map.containMarker(Map.getBound(map), markersBound))
+    }
 
     return (
         <div id={containerId}>
@@ -60,8 +66,24 @@ const Tmap = ({ containerId, startPosition, arrivePosition }: props) => {
                     width: "300px",
                 }}
             />
+            <div>{JSON.stringify(mapDIV)}</div>
+            <button onClick={onClick}>onClick</button>
         </div>
+
     );
 }
 
 export default Tmap;
+
+
+
+interface position {
+    latitude?: number,
+    longitude?: number
+}
+
+interface props {
+    containerId: string
+    startPosition: position
+    arrivePosition: position
+}
--- client/src/script/Tmap.tsx
+++ client/src/script/Tmap.tsx
@@ -3,6 +3,9 @@ const { Tmapv3 } = window;
 
 const TmapObject = {
 
+
+
+
     initTmap: () => {
         return new Tmapv3.Map("TMapApp", {
             center: new Tmapv3.LatLng(37.56520450, 126.98702028),
@@ -24,9 +27,57 @@ const TmapObject = {
         map.setZoom(16);
     },
 
-    LatLng : (lat: number, lon: number) => {
+    LatLng: (lat: number, lon: number) => {
         return new Tmapv3.LatLng(lat, lon)
+    },
+
+    getBound: (map: any) => {
+        // return {
+        //     1 : map.getBounds()._ne._lat,
+        //     2 : map.getBounds()._ne._lat,
+        //     3 : map.getBounds()._sw._lat,
+        //     4 : map.getBounds()._sw._lat};	
+
+        return map.getBounds();
+        // 이 부분을 수정
+        // bound 객체 생성 후
+        // 비교
+        // map.getBounds().contains(bounds2);
+
+        // map.fitBounds(latLngBounds,margin);
+    },
+
+    initBound: (l: number, b: number, r: number, t: number) => {
+        return new Tmapv3.Bounds(l, b, r, t)
+    },
+
+    fitmap: (map: any, latLngBounds: any, margin: number) => {
+        map.fitBounds(latLngBounds, margin)
+    },
+
+    test: (map : any , centerLat : number, centerLon : number, startLat : number, startLon : number, endLat : number, endLon : number) => {
+
+        // 화면의 중앙 좌표 설정
+        var lonlat =  new Tmapv3.LatLng(centerLat,centerLon);
+        var latlngBounds = new Tmapv3.LatLngBounds(lonlat);
+
+        // 시작지점
+        var start =  new Tmapv3.LatLng(startLat,startLon);
+
+        // 도착지점
+        var end =  new Tmapv3.LatLng(endLat,endLon);
+
+        // 확장
+        latlngBounds.extend(start);
+        latlngBounds.extend(end);
+        // 화면 맞춤
+        map.fitBounds(latlngBounds, 50);
+    },
+
+    containMarker: (map: any, bound: any) => {
+        return map.getBounds().contains(bound);
     }
+
 }
 
 export default TmapObject;
