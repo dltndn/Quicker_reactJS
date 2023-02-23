a83c1cf89181f415714cd747ed082db8811d8804
dltndn <james98099@gmail.com>
2023-02-23 19:02:20 +0900

    fix: 오티 수정

--- server/index.js
+++ server/index.js
@@ -3,13 +3,6 @@ const app = express();
 const port = 3300
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> roadname
->>>>>>> 146dc51f055aa3df9df50f01cffc12fbbb37b3e9
     res.json({
         success: true,
     });
@@ -17,19 +10,11 @@ app.get('/', (req, res) => {
 
 app.get('/test', (req, res) => {
     res.send("test")
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
     res.json({success: true});
 });
 
 app.post('/register', (req, res) => {
     console.log(req.body)
->>>>>>> roadname
->>>>>>> roadname
->>>>>>> 146dc51f055aa3df9df50f01cffc12fbbb37b3e9
 });
 
 app.listen(port, () => {
