c4c221f858c69894c1209f0952ba6158d3edbf72
Daniel-k-dev <juyeon10120@naver.com>
2023-03-03 06:41:33 +0900

    change : change DBconnector to sequelizeConnector

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,5 @@
 import express, { Application, Request, Response } from "express";
-import connector from "./DataBaseConnector";
-import sequelize from "./DBConnector";
+import sequelize from "./sequelizeConnector";
 
 const cors = require("cors");
 const app: Application = express();
@@ -11,23 +10,6 @@ const { Sequelize, DataTypes, Op } = require("sequelize");
 app.use(cors());
 app.use(bodyParser.json());
 
-// async function asyncFunction() {
-//   let conn;
-//   try {
-//     conn = await connector.getConnection();
-//     const rows = await conn.query("SELECT * FROM Quicker.User;");
-//     console.log(rows); //[ {val: 1}, meta: ... ]
-//     const res = await conn.query(
-//       "INSERT INTO Quicker.User (wallet_adress, name, email, contact) VALUES (?, ?, ?, ?)",
-//       ["walletadress", "username", "emailadress", "contact"]
-//     );
-//     console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-//   } catch (err) {
-//     throw err;
-//   } finally {
-//     if (conn) return conn.end();
-//   }
-// }
 
 let connectDBWithsequelize = async () => {
   try {
