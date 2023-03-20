25d7ceb5ebf96473e75f65a49ef4dbe714c95d3d
Daniel-k-dev <juyeon10120@naver.com>
2023-03-20 15:16:33 +0900

    feat : when user location detected, initialize marker and move map to users position

--- client/src/lib/Geolocation.tsx
+++ client/src/lib/Geolocation.tsx
@@ -3,7 +3,7 @@ export default {
         if (navigator.geolocation) {
     
             navigator.geolocation.getCurrentPosition(function(position) {
-                    setState({lat : position.coords.latitude, lon : position.coords.longitude})
+                setState({lat : position.coords.latitude, lon : position.coords.longitude})
             });
           } else { 
               alert("위치를 사용할 수 없음")
--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -2,18 +2,33 @@ import React, { useEffect, useState } from "react";
 import BottomBar from "../components/BottomBar";
 import TopBarOthers from "../components/topBarOthers"
 import { useNavigate } from "react-router-dom";
-import Tmap from "../lib/Tmap";
+import Map from "../lib/Tmap"
 import Geolocation from "../lib/Geolocation";
 
 function SearchPage() {
+  
   const navigate = useNavigate()
   const [map, setMap] = useState({})
   const [userLocation, setUserLocation] = useState({})
+  
+  let initalizeUserMarker = () => { 
+    // @ts-ignore
+    let pos = Map.LatLng(userLocation.lat,userLocation.lon)
+    // @ts-ignore
+    Map.Marker(map, userLocation.lat,userLocation.lon)
+    Map.panTo(map, pos)
+  }
 
   useEffect(() => {
-    setMap(Tmap.initTmap());
-    Geolocation.getCurrentLocation(setUserLocation)
+    setMap(Map.initTmap());
+    Geolocation.getCurrentLocation(setUserLocation)    
   }, [])
+
+  useEffect(() => {
+    if (Object.keys(userLocation).length !== 0) {
+      initalizeUserMarker()
+    }
+  }, [userLocation])
 
   return (
     <div>
