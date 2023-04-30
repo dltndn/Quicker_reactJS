a8ec0110419a05fa9f09949884fca6b6472294b5
Daniel-k-dev <juyeon10120@naver.com>
2023-05-01 03:44:55 +0900

    pull : pull upstream develop

---
+++ server/models/Commands/CreateChatRoom.ts
@@ -0,0 +1,16 @@
+import { Chat_room } from "../DB/init-models";
+
+export = {
+  createChatRoom: (orderId: number, deliverId: string, requesterId: string) => {
+    return new Promise(async (resolve, reject) => {
+      await Chat_room.create({
+        chat_order_id: orderId,
+        request_id: requesterId,
+        delivery_id: deliverId,
+      });
+
+      resolve("done");
+      reject("fail");
+    });
+  },
+};
---
+++ server/models/Commands/CreateOrder.ts
@@ -0,0 +1,17 @@
+import {Order,Transportation,Destination,Departure,Product,Sender,Recipient} from "../DB/init-models";
+
+export = {
+  Order: (data: any) => {
+    return new Promise((resolve, reject) => {
+      Order.create(data.Order);
+      Transportation.create(data.Transportation);
+      Destination.create(data.Destination);
+      Departure.create(data.Departure);
+      Product.create(data.Product);
+      Sender.create(data.Sender);
+      Recipient.create(data.Recipient);
+      resolve("done");
+      reject("fail");
+    });
+  },
+};
