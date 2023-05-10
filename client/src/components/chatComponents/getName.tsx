485e4f0da61ab8750cc7a4cd66b4d52037d1525e
Daniel-k-dev <juyeon10120@naver.com>
2023-05-10 10:52:27 +0900

    fix : filtering not my order in chat page

--- client/src/components/chatComponents/getName.tsx
+++ client/src/components/chatComponents/getName.tsx
@@ -1,27 +1,23 @@
 import { SetStateAction } from "react"
 import Handler from "../../lib/Handler"
 import { getNameInterface } from "./interface/getNameInterface"
+import delelteDoubleQuote from "./DeleteDoubleQuote"
 
 const getName = async ({ blockchainElement, address, setState }: getNameInterface) => {
     let clientWalletAddress = delelteDoubleQuote(blockchainElement.client)
     if (clientWalletAddress === address) {
         const walletAddress = delelteDoubleQuote(blockchainElement.quicker)
-        console.log("사용자의 접속된 지갑 주소 : " ,address)
-        console.log("요청하는 블록체인 지갑 주소 : ", walletAddress)
         const data = await Handler.post({ walletAddress: walletAddress }, process.env.REACT_APP_SERVER_URL + "getUserNameUseByWalletAddress")
         isMember(data, setState)
     }
     else {
         const walletAddress = delelteDoubleQuote(blockchainElement.client)
-        console.log("사용자의 접속된 지갑 주소 : " ,address)
-        console.log("요청하는 블록체인 지갑 주소 : ", walletAddress)
         const data = await Handler.post({ walletAddress: walletAddress }, process.env.REACT_APP_SERVER_URL + "getUserNameUseByWalletAddress")
         isMember(data, setState)
     }
 }
 
 const isMember = (data: { name: string | null }, setState: React.Dispatch<SetStateAction<string>>): void => {
-    console.log("받는 데이터 :",data)
     if (isNameNull(data)) {
         setState("탈퇴한 회원입니다.");
     } else if (data.name) {
@@ -36,10 +32,6 @@ const isNameNull = (data: { name: null | undefined | string; }): boolean => {
     else {
         return false
     }
-}
-
-const delelteDoubleQuote = (blockChainAddress : string) => {
-    return blockChainAddress.substring(1,blockChainAddress.length-1)
 }
 
 export default getName
