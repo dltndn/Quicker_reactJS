3dada110547deaae50a4c218596622cf05a0b823
dudwns0213 <101815958+dudwns0213@users.noreply.github.com>
2023-05-17 03:49:37 +0900

    feat: 채팅 페이지 제작

--- client/src/components/Notification.tsx
+++ client/src/components/Notification.tsx
@@ -2,7 +2,12 @@ import TopBarThin from './topBarthin';
 import styled, { createGlobalStyle } from 'styled-components';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { BsChatLeftDots } from "react-icons/bs";
+import { BsChatLeftDots, BsTelephone, BsStickies } from "react-icons/bs";
+
+import { HiPaperAirplane } from "react-icons/hi2";
+
+const Chatdot = require('../image/Chatdot.png')
+const Chatman = require('../image/Chatman.png')
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -10,7 +15,7 @@ const GlobalStyle = createGlobalStyle`
     height: 100%;
   }
 `;
-
+/* 
 const SelectInput = styled.select`
   width: 6rem;
   height: 1.625rem;
@@ -62,6 +67,7 @@ const Div1_3 = styled.div`
   padding: 4px 0px 4px 0px;
   font-weight: thin;
 `;
+*/
 
 function Notification() {
   const navigate = useNavigate();
@@ -73,7 +79,7 @@ function Notification() {
 
   return (
     <>
-      <GlobalStyle />
+      {/* <GlobalStyle />
       <TopBarThin
         title="알림"
         title2=""
@@ -121,9 +127,114 @@ function Notification() {
             <Div1_3>3월 20일</Div1_3>
           </Div1>
         ) : null}
-      </Se0>
+      </Se0> */}
+      <TopBarThin
+        title="알림"
+        title2=""
+        redirectLogic={() => {
+          navigate("/");
+        }}
+      />
+      <Div0><Div1>김포시 김포대로 926번길 46 <Div2><BsTelephone/> 010-9086-9502 <BsStickies/></Div2></Div1>
+      <div><Img1 src={Chatdot}></Img1></div>
+      <Div1>김포시 김포대로 926번길 46 <Div2><BsTelephone/> 010-9086-9502 <BsStickies/></Div2></Div1></Div0>
+      <Div3><Img2 src={Chatman}></Img2> <DivChat><span>대화내용 1</span></DivChat> <Divclock>오전 00:00</Divclock></Div3>
+      <Divdate>0000년 00월 00일</Divdate>
+      <Div4><Divclock>오전 00:00</Divclock><DivChat2><span>대화내용 1</span></DivChat2></Div4>
+			<Sc0><div><Ip1>메세지 보내기</Ip1></div><HiPaperAirplane/></Sc0>
     </>
   );
 }
 
 export default Notification;
+
+const Div0 = styled.div`
+	margin: 0px 10px 5px 10px;
+	border-style: solid;
+  border-color: #efefef;
+  border-width: 0.5px 0px 0.5px 0px;
+`
+
+const Div1 = styled.div`
+  display: flex;
+  padding: 10px 10px 10px 10px;
+  flex-direction:column;
+  font-size: 14px;
+  font-weight: bold;
+`
+
+const Div2 = styled.div`
+  font-size: 12px;
+  font-weight: thin;
+  color: #646464;
+`
+
+const Img1 = styled.img`
+  width: 4px;
+  margin: -5px 0px -5px 90px;
+
+`
+
+const Img2 = styled.img`
+  width: 33px;
+	height: 33px;
+`
+
+const Div3 = styled.div`
+  display: flex;
+	margin: 10px 10px 10px 15px;
+`
+const DivChat = styled.div`
+	background-color: #f8f8f8;
+	border-radius: 20px;
+	justify-content: center;
+  align-items: center;
+  display: flex;
+	margin: 0px 5px 0px 10px;
+	padding: 8px;
+`
+const Divclock = styled.div`
+	display: flex;
+	align-items: flex-end;
+	font-size: 8px;
+	font-weight: thin;
+	color: #adadad;
+`
+
+const Divdate = styled.div`
+	width: 100%;
+	margin: 10px 0 10px 0;
+	display: flex;
+	justify-content: center;
+	font-size: 8px;
+	font-weight: thin;
+	color: #adadad;
+`
+const Div4 = styled(Div3)`
+	justify-content: flex-end;
+`
+
+const DivChat2 = styled(DivChat)`
+	background-color: #79AFFF;
+	color:#ffffff;
+`
+
+const Sc0 = styled.section`
+    position: fixed;
+    display: flex;
+    bottom: 0;
+    width: 100%;
+    height: 3.875rem;
+    background-color: var(--white-color); 
+    align-items: center;
+    justify-content: center;
+    text-align:center;
+`;
+
+const Ip1 = styled.input`
+	border-radius: 20px;
+	background-color: #e6e6e6;
+	outline: none;
+	height: auto;
+	color: #A0A0A0;
+`
