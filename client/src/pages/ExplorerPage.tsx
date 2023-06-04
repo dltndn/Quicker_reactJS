404deb187a318486480244bb62fb6b8fc3252745
dltndn <james98099@gmail.com>
2023-06-05 01:27:47 +0900

    fix: Explorer페이지 오더 노출 순서 오류 수정

--- client/src/pages/ExplorerPage.tsx
+++ client/src/pages/ExplorerPage.tsx
@@ -89,7 +89,8 @@ export default function ExplorerPage() {
         const result: any = await getOrdersForLatest(amount.toString());
         // 바뀐 부분 확인
         const newOrderArr = result.slice()
-        if (newOrderArr.length <= 20) {
+        console.log(newOrderArr.length)
+        if (newOrderArr.length < 20) {
           newOrderArr.reverse()
         }
         let changedIndex:number[] = []
