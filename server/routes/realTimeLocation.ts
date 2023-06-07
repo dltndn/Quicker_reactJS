de8def0c6bcffa495bcde5ff5b9cb568a02dacfe
Daniel-k-dev <juyeon10120@naver.com>
2023-06-07 10:40:40 +0900

    fix : 기존 실시간 위치 처리 라우터 post요청 함수 동적 수정

--- server/routes/realTimeLocation.ts
+++ server/routes/realTimeLocation.ts
@@ -22,9 +22,9 @@ router
   })
   .post("/", async (req: Request, res: Response) => {
     try {
-      const address = "0x2cc285279f6970d00f84f3034439ab8d29d04d97";
-      const tempY = 37.53467664139352;
-      const tempX = 127.2130254778349;
+      const address = req.body.address;
+      const tempY = req.body.Y;
+      const tempX = req.body.X;
 
       const conn = await connectMongo("realTimeLocation");
       const realLocationSchema = conn.model(address, RealLocationSchema);
@@ -42,30 +42,3 @@ router
   });
 
 export default router;
-
-// import { Request, Response } from "express";
-// import connectMongo from "../Mongo/Connector";
-// import RealLocationSchema from "../Mongo/Schemas/realLocation";
-
-// const realTimeLocation = async (req: Request, res: Response) => {
-//   try {
-//     const address = "지갑주소"
-//     const tempX = 37.4
-//     const tempY = 128.4
-
-//     const conn = await connectMongo("realTimeLocation");
-//     const realLocationSchema = conn.model(address, RealLocationSchema);
-//     const realLocationModel = new realLocationSchema ({
-//         X : tempX,
-//         Y : tempY
-//     })
-//     await realLocationModel.save();
-
-//     res.send({ msg: "done" });
-//   } catch (error) {
-//     console.log(error)
-//     res.send({msg : "fail"})
-//   }
-// };
-
-// export default realTimeLocation;
