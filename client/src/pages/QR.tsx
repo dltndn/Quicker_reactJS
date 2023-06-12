9c1565683a8f9befb2b17ee9df8c573f873cb4bf
Daniel-k-dev <juyeon10120@naver.com>
2023-06-12 15:41:45 +0900

    test : QR코드 테스트 예제

--- client/src/pages/QR.tsx
+++ client/src/pages/QR.tsx
@@ -2,44 +2,69 @@
 import { Html5QrcodeScanner } from "html5-qrcode";
 
 // To use Html5Qrcode (more info below)
-// import { Html5Qrcode } from "html5-qrcode";
+import { Html5Qrcode } from "html5-qrcode";
 import { useEffect, useState } from "react";
 
 export default function QR() {
 
     const [scanResult, setScanResult] = useState(null)
+    const [html5QrCode, setHtml5QrCode] = useState<Html5Qrcode | null>(null)
+
+    const onclick = () => {
+        if (html5QrCode !== null) {
+            html5QrCode.stop().then((ignore) => {
+                // QR Code scanning is stopped.
+                console.log(ignore)
+            }).catch((err: any) => {
+                // Stop failed, handle it.
+                console.log(err)
+            })
+        }
+    }
 
     useEffect(() => {
-        const scanner = new Html5QrcodeScanner("reader", {
-            qrbox: {
-                width: 250,
-                height: 250,
-            }, 
-            fps: 5
-        }, undefined)
+        if (html5QrCode !== null) {
+            const qrCodeSuccessCallback = (decodedText: any, decodedResult: any) => {
+                alert(JSON.stringify(decodedResult))
+            };
+            const qrCodeErrorCallback = (decodedText: any, decodedResult: any) => {
+                /* handle success */
+            };
+            const config = { fps: 10, qrbox: { width: 250, height: 250 } };
     
-        scanner.render(success, error);
-    
-        function success (result : any) {
-            scanner.clear();
-            setScanResult(result)
-            console.log(result)
+            // If you want to prefer back camera
+            html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback, qrCodeErrorCallback);
         }
-    
-        function error (error : any) {
-            console.log(error)
-        }
-    
-    }, [])
-    
-    return (
         
-        <>
-            <h1>QR Code Scanning in React</h1>
-            { scanResult
-            ? <div>Success: <a href={'http://'}></a></div>
-            : <div id="reader"></div>
+    }, [html5QrCode])
+
+    useEffect(() => {
+        Html5Qrcode.getCameras().then(devices => {
+            /**
+             * devices would be an array of objects of type:
+             * { id: "id", label: "label" }
+             */
+            if (devices && devices.length) {
+                var cameraId = devices[0].id;
+                // .. use this to start scanning.
+                console.log(cameraId)
+
+                setHtml5QrCode(new Html5Qrcode("reader"))
+
             }
+        }).catch(err => {
+            // handle err
+        });
+
+    }, [])
+
+    return (
+        <>
+            {scanResult
+                ? <div>Success: {scanResult}</div>
+                : <div id="reader"></div>
+            }
+            <button onClick={onclick}>stop</button>
         </>
     )
 }
