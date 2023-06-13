ac375460d30f63b69641c27e16d31d4fc03dce8e
Daniel-k-dev <juyeon10120@naver.com>
2023-06-13 15:02:40 +0900

    feat : 기본 메세지 템플릿 작성

--- server/sendMessage.ts
+++ server/sendMessage.ts
@@ -1,14 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = require("twilio")(accountSid, authToken);
 
-const sendURLToReceiver = async (editedPhoneNumber : String, url : String) => {
+const messageTemplate = `[Quicker]\n\n반갑습니다, 고객님.\n고객님의 소중한 상품이 배송 예정입니다.\n\n※ 실시간 배송정보\n`;
+
+const sendURLToReceiver = async (editedPhoneNumber: String, url: String) => {
   try {
     const message = await client.messages.create({
-      body: url,
+      body: messageTemplate + url,
       from: process.env.TWILIO_PHONE_NUMBER,
       to: "+82" + editedPhoneNumber,
     });
@@ -17,6 +19,6 @@ const sendURLToReceiver = async (editedPhoneNumber : String, url : String) => {
     // You can implement your fallback code here
     console.error(error);
   }
-}
+};
 
 export default sendURLToReceiver;
