ad11139bbcd8d43c208e53741d9df6ed8bc57cb5
Daniel-k-dev <juyeon10120@naver.com>
2023-06-14 15:33:37 +0900

    fix : 기존 crypto 수정

--- server/lib/cryto.ts
+++ server/lib/cryto.ts
@@ -1,30 +1,14 @@
-import crypto from "crypto"
+const CryptoJS = require("crypto-js");
 
 require("dotenv").config();
 
-const algorithm = 'aes-256-gcm';
-const iv = crypto.randomBytes(16); // 초기화 벡터 생성
-const key =  process.env.URL_CRYPTO_KEY
-
 // 암호화 메서드
 export function encrypt(text: string): string {
-    if (typeof key !== "undefined") {
-        const cipher = crypto.createCipheriv(algorithm, key, iv);
-        let encrypted = cipher.update(text, 'utf8', 'hex');
-        encrypted += cipher.final('hex');
-        return encrypted;
-    }
-    return "fail"
-  }
+  return CryptoJS.AES.encrypt(text, process.env.URL_CRYPTO_KEY).toString();
+}
 
 // 복호화 메서드
 export function decrypt(encryptedText: string): string {
-    if (typeof key !== "undefined") {
-        const decipher = crypto.createDecipheriv(algorithm,key,iv)
-        let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
-        return decrypted;
-    }
-    return "fail"
+  const bytes = CryptoJS.AES.decrypt(encryptedText, process.env.URL_CRYPTO_KEY);
+  return bytes.toString(CryptoJS.enc.Utf8);
 }
-
-
