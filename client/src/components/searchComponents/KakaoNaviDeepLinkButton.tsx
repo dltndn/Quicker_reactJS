46f1157a3562604cdffa49b8c03f5963ebf1161e
Daniel-k-dev <juyeon10120@naver.com>
2023-06-17 00:53:06 +0900

    fix : 이미지 오류 수정

--- client/src/components/searchComponents/KakaoNaviDeepLinkButton.tsx
+++ client/src/components/searchComponents/KakaoNaviDeepLinkButton.tsx
@@ -1,5 +1,5 @@
 import { OnePointRoute, TwoPointRoute } from "./interface/route";
-
+import kakaoNaviIcon from './kakaonavi_btn_medium.png';
 // @ts-ignore
 const { Kakao } = window
 
@@ -14,6 +14,6 @@ export default function KakaoNaviDeepLinkButton(prop: Prop) {
     }
 
     return (
-        <img src="https://developers.kakao.com/assets/img/about/buttons/navi/kakaonavi_btn_medium.png" alt="목적지 공유하기 버튼" onClick={() => { startNavigation(routeInfo) }} width={"20px"} height={"20px"} />
+        <img src={kakaoNaviIcon} alt="카카오 Deeplink 버튼" onClick={() => {startNavigation(routeInfo) }} width={"20px"} height={"20px"} />
     )
 }
