bca71c9d06b0737f33872c924a17916c1763381a
Daniel-k-dev <juyeon10120@naver.com>
2023-06-17 00:40:08 +0900

    fix : 카카오네비 Deeplink 버튼 이미지 수정, 이미지 사이즈 조절 필요

--- client/src/components/searchComponents/KakaoNaviDeepLinkButton.tsx
+++ client/src/components/searchComponents/KakaoNaviDeepLinkButton.tsx
@@ -14,8 +14,6 @@ export default function KakaoNaviDeepLinkButton(prop: Prop) {
     }
 
     return (
-        <>
-            <button onClick={() => { startNavigation(routeInfo) }}>카카오 링크</button>
-        </>
+        <img src="https://developers.kakao.com/assets/img/about/buttons/navi/kakaonavi_btn_medium.png" alt="목적지 공유하기 버튼" onClick={() => { startNavigation(routeInfo) }} width={"20px"} height={"20px"} />
     )
 }
