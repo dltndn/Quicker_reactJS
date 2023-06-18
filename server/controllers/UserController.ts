ba78d30bb148a24c6dae390fb102e2c9aa97f416
Daniel-k-dev <juyeon10120@naver.com>
2023-06-18 10:07:21 +0900

    test : 회원가입 오류 테스트

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -26,9 +26,11 @@ export default {
 
   register: async (req: Request, res: Response) => {
     try {
+      console.log('test')
       const secret = process.env.cryptoKey;
       const userInstance = req.body.User;
       const userBirthDate = req.body.Birthday;
+      console.log(req.body)
       //NOTE : 전화번호를 기반으로 암호화한 id 사용
       const hashed = crypto
         .createHmac("sha256", secret)
@@ -37,6 +39,7 @@ export default {
       userInstance.id = hashed;
       userBirthDate.id = hashed;
       await CreateUser.registerUser(userInstance, userBirthDate, hashed);
+      console.log('done')
       res.send({ msg: "done" });
     } catch (error) {
       console.log(error)
