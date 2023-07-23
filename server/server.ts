b42846372d90e655d79ba358b7045bdda4ed2461
Daniel-k-dev <juyeon10120@naver.com>
2023-07-23 13:26:25 +0900

    refactor : 서버 포트 설정 코드 리팩토링

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,14 @@
-import express, { Application } from "express";
+import express, { Application, Response, Request, NextFunction } from "express";
 
 // 필요한 모듈 import
 import chat from "./chat/socket";
 import loader from "./loaders/loader";
 import router from "./loaders/express";
+
+const initializePort = (app : Application) => {
+    const HTTP_PORT = (process.env.NODE_ENV === "development") ? process.env.HTTP_LOCAL_SERVER_PORT : process.env.HTTP_AWS_SERVER_PORT;
+    return app.listen(HTTP_PORT, () => console.log(`App is listening on port ${HTTP_PORT} !`));    
+}
 
 const startServer = async () => {
     const app: Application = express();
@@ -12,8 +17,8 @@ const startServer = async () => {
 
     await router.handle(app);
     
-    const HTTP_PORT = (process.env.NODE_ENV === "development") ? process.env.HTTP_LOCAL_SERVER_PORT : process.env.HTTP_AWS_SERVER_PORT;
-    const server = app.listen(HTTP_PORT, () => console.log(`App is listening on port ${HTTP_PORT} !`));    
+    const server = initializePort(app)
+    
     chat(server)
 }
 
