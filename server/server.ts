d1e891236b544d91e89c3e3e961d61396821ce54
Daniel-k-dev <juyeon10120@naver.com>
2023-09-22 03:57:14 +0900

    refactor : 서버 포트 할당 부분 추출

--- server/server.ts
+++ server/server.ts
@@ -1,23 +1,18 @@
-import express, { Application } from "express";
+import express from "express";
 
-// 필요한 모듈 import
 import chat from "./chat/socket";
-import loader from "./loaders/loader";
 import router from "./loaders/express";
+import loader from "./loaders/loader";
+import port from "./loaders/port";
 
-const initializePort = (app : Application) => {
-    const HTTP_PORT = (process.env.NODE_ENV === "development") ? process.env.HTTP_LOCAL_SERVER_PORT : process.env.HTTP_AWS_SERVER_PORT;
-    return app.listen(HTTP_PORT, () => console.log(`App is listening on port ${HTTP_PORT} !`));    
-}
-
-const startServer = async () => {
+const startServer = () => {
     const app = express();
 
     loader.init(app);
 
-    await router.handle(app);
+    router.handle(app);
     
-    const server = initializePort(app)
+    const server = port.init(app)
     
     chat(server)
 }
