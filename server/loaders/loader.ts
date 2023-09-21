799cea9d1df68b2afd2848b708f15460543d2076
Daniel-k-dev <juyeon10120@naver.com>
2023-09-22 03:45:46 +0900

    refactor : loader 리팩토링

--- server/loaders/loader.ts
+++ server/loaders/loader.ts
@@ -1,25 +1,17 @@
+import bodyParser from "body-parser";
 import compression from "compression";
-import express, { Application, NextFunction } from "express";
-import * as fs from "node:fs";
-import path from "node:path";
+import cors from "cors";
+import express, { Application } from "express";
 
-import { formatDateToYYYYMMDD } from "../util/dateFormat";
-import { customMorgan } from "./middlewares/customMorgan";
-import { calculateCostAverageInEveryMonth } from "./middlewares/cronJob";
+import { insertAverageCostPerMonth } from "./middlewares/cron-job";
+import { logger } from "./middlewares/morgan-config";
+
 const loader = {
   init : (app: Application) => {
 
-    calculateCostAverageInEveryMonth
-    const cors = require("cors");
-    const bodyParser = require("body-parser");
-    let writeStream : any;
-    app.use((req, res,next : NextFunction) => {
-      writeStream = fs.createWriteStream(path.join(__dirname, `../logs/${formatDateToYYYYMMDD(new Date())}.log`), {flags : "a"});
-      next()
-    })
-    const logWriter = {write : (log : string) => writeStream.write(log)}
-
-    app.use(customMorgan(':custom', {stream : logWriter}))
+    insertAverageCostPerMonth
+    
+    app.use(logger())
     app.use(compression());
     app.use(cors());
     app.use(express.urlencoded({ extended: true }));
