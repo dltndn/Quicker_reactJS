1e6924ffdac6ec36ebf43eb71eee083166e996b3
Daniel-k-dev <juyeon10120@naver.com>
2023-10-01 04:15:21 +0900

    fix : mongo/command/message findRecent 메서드 사용 방법 통일에 따른 변경

--- server/controllers/chat.ts
+++ server/controllers/chat.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from "express";
-import { findRecentMessage } from "../../room";
+import { messageInstance } from "../mongo/command";
 
 export class ChatController {
   async getRecentMessage (req: Request, res: Response, next: NextFunction) {
     try {
-      const query = req.query;
-      const recentMessage = await findRecentMessage(query)
+      const orderId = req.query.orderNum;
+      if (typeof orderId !== "number") throw new Error ("orderId not exist")
+      const recentMessage = await messageInstance.findRecent(orderId)
       res.send(recentMessage);
     } catch (error) {
       console.error(error);
