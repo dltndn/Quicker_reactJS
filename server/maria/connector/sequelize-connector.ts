7cbf303c98100a20a0931dd8786b6b04aa976d93
Daniel-k-dev <juyeon10120@naver.com>
2023-10-01 04:12:13 +0900

    refactor : config를 이용하여 키값 import

--- server/maria/connector/sequelize-connector.ts
+++ server/maria/connector/sequelize-connector.ts
@@ -1,11 +1,10 @@
-require("dotenv").config();
+import config from "../../config";
 
 const { Sequelize } = require("sequelize");
-const env : NodeJS.ProcessEnv = process.env;
 
-export default new Sequelize("Quicker", env.user, env.password, {
+export default new Sequelize("Quicker", config.db.maria.user, config.db.maria.password, {
   dialect: "mariadb",
-  host: env.host,
-  port: env.port,
+  host: config.db.maria.host,
+  port: config.db.maria.port,
   logging: false
 });
