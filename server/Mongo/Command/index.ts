2d73cbe021e5ac74a432974a289a6f9c44dcc704
Daniel-k-dev <juyeon10120@naver.com>
2023-10-04 16:34:46 +0900

    delete : git  내부 파일 이름 변경을 위한 일시 삭제

--- server/Mongo/Command/index.ts
+++
@@ -1,9 +0,0 @@
-import { ImageModel } from "./image";
-import { CurrentLocationModel } from "./location";
-import { MessageModel } from "./message";
-
-const imageInstance = new ImageModel();
-const currentLocationInstance = new CurrentLocationModel();
-const messageInstance = new MessageModel();
-
-export { imageInstance, currentLocationInstance, messageInstance };
