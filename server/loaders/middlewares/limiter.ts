4992f0fcd1dc1b4522086dc107c809ef5dee4455
Daniel-k-dev <juyeon10120@naver.com>
2023-10-21 14:08:22 +0900

    fix : express limiter CommonJS -> ESM 방식으로 변경

--- server/loaders/middlewares/limiter.ts
+++ server/loaders/middlewares/limiter.ts
@@ -1,10 +1,11 @@
-const rateLimit = require('express-rate-limit');
+import { Request, Response } from 'express';
+import { rateLimit } from 'express-rate-limit'
 
 // Rate limiter 설정
 const caverLimiter = rateLimit({
   windowMs: 1 * 1000, // 1 seconds
   max: 20, // limit each IP to 50 requests per windowMs
-  handler(_: any, res: any) {
+  handler(req: Request, res: Response) {
     res.status(204).json({ errorMsg: 'Too many requests' });
   },
 });
